11e6db6a5e83422ae2a086192135a498ce2730a7
Dmitry Starina <dimastarina@gmail.com>
2021-07-29 17:00:11 +0300

    added menu items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './components/home-page/home-page.component';
+import HomePage from './pages/home-page/home-page.component';
 
 function App() {
   return (
